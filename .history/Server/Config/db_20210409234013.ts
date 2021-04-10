7f483f8f6d6dfef7559fd2e2ec74f2e5c2771625
Yiu Wing Lau <yiuwing.lau@dcmail.ca>
2021-04-10 14:57:46 -0400

    final commit

--- .history/Server/Config/db_20210409234013.ts
+++ .history/Server/Config/db_20210409234013.ts
@@ -3,7 +3,7 @@ export const localURI = "mongodb://localhost/webd6201";
 export const Secret = "someSecret";
 
 // remote Access
-export const RemoteURI = "mongodb+srv://Nelson:zRftBl5kOEbLVr2O@webd6201.kbwai.mongodb.net/WEBD6201?retryWrites=true&w=majority";
+export const RemoteURI = "mongodb+srv://thomas:0xHN3GappBBYrAfh@cluster0.thzq9.azure.mongodb.net/webd6201?retryWrites=true&w=majority";
 export const ConnectionType = "MongoDB Atlas";
 
 
